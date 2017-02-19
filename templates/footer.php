680df22bfc78f02e459928ddd75f6294875c5e8c
Jonathan Sigg <jonathan.sigg@hotmail.com>
2017-02-19 20:36:30 +0100

    update

--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,24 @@
 <footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-xs-12 col-md-4">
+        <h4>Soziale Medien</h4>
+        <p>
+          <nav class="nav nav-inline">
+            <a class="nav-link" href="/"><i class="fa fa-facebook fa-2x"></i></a>
+            <a class="nav-link" href="/"><i class="fa fa-instagram fa-2x"></i></a>
+          </nav>
+        </p>
+      </div>
+      <div class="col-xs-12 col-md-4">
+        <div class="form-grpup">
+          <h4>Empfehlen sie uns weiter</h4>
+          <div id="TA_restaurantWidgetGreen377" class="TA_restaurantWidgetGreen"><ul id="Dl1CitU" class="TA_links rIWtzyOR0"><li id="SZcluTWbB" class="HWBknF"><a target="_blank" href="https://www.tripadvisor.ch/"><img src="https://www.tripadvisor.ch/img/cdsi/partner/tripadvisor_logo_117x18-24177-2.png" alt="TripAdvisor"/></a></li></ul></div><script src="https://www.jscache.com/wejs?wtype=restaurantWidgetGreen&amp;uniq=377&amp;locationId=11916211&amp;icon=knifeAndFork&amp;lang=de_CH&amp;display_version=2"></script>
+        </div>
+      </div>
+    </div>
+    <div class="small">
+      Italian Place &copy; - 2017
+    </div>
   </div>
 </footer>
